d42b217f699a02e939f415ffd0d05d6d9db88963
PharrellWANG <15113029g@connect.polyu.hk>
2017-08-18 22:01:23 +0800

    updates

--- 3_count.py
+++ 3_count.py
@@ -6,6 +6,6 @@ import os
 from utils.mode_counter import mode_counter
 
 homedir = os.environ['HOME']
-input_file = homedir + '/data/step2_output/size_16_files.csv'
-
+# input_file = homedir + '/data/step2_output/size_32_files.csv'
+input_file = '/Users/pharrell_wang/data/step3_output/32x32/data_for_training/train_32x32.csv'
 x_ordered_dict = mode_counter(INPUT_FILE=input_file)
