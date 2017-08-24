1b5365e6b14769d93725c3577c03325d5c234896
PharrellWANG <15113029g@connect.polyu.hk>
2017-08-24 15:47:28 +0800

    32x32

--- 3_count.py
+++ 3_count.py
@@ -6,5 +6,6 @@ import os
 from utils.mode_counter import mode_counter
 
 homedir = os.environ['HOME']
-input_file = homedir + '/data/step2_output/size_32_files.csv'
+# input_file = homedir + '/data/step2_output/size_32_files.csv'
+input_file = homedir + '/data/step3_output/32x32/data_for_validating/validate_32x32.csv'
 x_ordered_dict = mode_counter(INPUT_FILE=input_file)
