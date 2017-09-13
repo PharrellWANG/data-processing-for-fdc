a2bdcf1158076d89a1d11580c8236de0b5504983
Pharrell.zx <wzxnuaa@gmail.com>
2017-09-13 18:10:42 +0800

    ..

--- x3_grouping.py
+++ x3_grouping.py
@@ -61,11 +61,52 @@ for file_index in range(len(input_files)):
 	number_of_rows = len(df.index)
 	print('number_of_rows : ' + str(number_of_rows))
 	
+	cnt_2 = 0
+	cnt_3 = 0
+	cnt_4 = 0
+	cnt_31 = 0
+	cnt_32 = 0
+	cnt_33 = 0
 	for x in range(number_of_rows):
 		if x % 50 == 0:
 			sys.stdout.write('\r>> processing line: %d/%d' % (x, number_of_rows))
-		if df[64][x] == 2 or df[64][x] == 3 or df[64][x] == 4 or df[64][x] == 31 or df[64][x] == 32 or df[64][x] == 33:
+		# for mixing 6 classes better
+		if df[64][x] == 2 and cnt_2 < 1836:
+			cnt_2 += 1
 			df[64][x] = 0
+		elif df[64][x] == 2 and cnt_2 >= 1836:
+			df[64][x] = 99
+			
+		elif df[64][x] == 3 and cnt_3 < 1836:
+			cnt_3 += 1
+			df[64][x] = 0
+		elif df[64][x] == 3 and cnt_3 >= 1836:
+			df[64][x] = 99
+			
+		elif df[64][x] == 4 and cnt_4 < 1836:
+			cnt_4 += 1
+			df[64][x] = 0
+		elif df[64][x] == 4 and cnt_4 >= 1836:
+			df[64][x] = 99
+			
+		elif df[64][x] == 31 and cnt_31 < 1836:
+			cnt_31 += 1
+			df[64][x] = 0
+		elif df[64][x] == 31 and cnt_31 >= 1836:
+			df[64][x] = 99
+			
+		elif df[64][x] == 32 and cnt_32 < 1836:
+			cnt_32 += 1
+			df[64][x] = 0
+		elif df[64][x] == 32 and cnt_32 >= 1836:
+			df[64][x] = 99
+			
+		elif df[64][x] == 33 and cnt_33 < 1836:
+			cnt_33 += 1
+			df[64][x] = 0
+		elif df[64][x] == 33 and cnt_33 >= 1836:
+			df[64][x] = 99
+			
 		elif df[64][x] == 5:
 			df[64][x] = 1
 		elif df[64][x] == 6:
